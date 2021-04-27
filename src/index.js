0291c8c8c7b209fc733ff88a2eaa375d5c41fbbf
Saud Topcagic <Sersto@byggtjeneste.no>
2021-04-27 17:53:54 +0200

    movies wrapper

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import axios from "axios";
+import searchMovies from "./Movies/searchMovieTitles"
+import top250 from "./Movies/top250Movies"
+import mostPopular from "./Movies/mostPopular"
+import inTheaters from "./Movies/inTheaters"
+import commingSoon from "./Movies/commingSoon"
 
 dotenv.config();
 const app = express()
 const port = process.env.PORT
+const APikey = process.env.APikey
+
 
 app.get('/', async (req, res) => {
-
   res.send('Test')
 })
 
-app.listen(port, () => console.log(`Slušam na portu ${port}!`))
+app.use("/movies", searchMovies)
+app.use("/movies", top250)
+app.use("/movies", mostPopular)
+app.use("/movies", inTheaters)
+app.use("/movies", commingSoon)
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
 
 
