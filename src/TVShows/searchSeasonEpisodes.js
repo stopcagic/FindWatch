064c3e100260e1f22f6abd538620924629f2caa5
Saud Topcagic <Sersto@byggtjeneste.no>
2021-05-06 21:37:35 +0200

    add info endpoint, utils, Models and fix searchSeasons

--- src/TVShows/searchSeasonEpisodes.js
+++ src/TVShows/searchSeasonEpisodes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import axios from "axios"
 import dotenv from "dotenv"
+import Utils from "../Utils/callEndpointUtil"
 
 dotenv.config();
 
@@ -14,27 +15,21 @@ router.get("/seasonEpisodes", async (req, res) => {
   let IMDbId = req.query.id;
   let data;
 
-  if (name) {
+  if (IMDbId != undefined) {
+    data = await Utils.searchEpisodes(IMDbId, season);
+  }
+  else if (name) {
     let SeriesTitle = await axios.get(`https://imdb-api.com/en/API/SearchSeries/${APikey}/${name}`);
     IMDbId = SeriesTitle.data.results[0].id;
 
-    data = await fetchSeason(IMDbId, season);
-  }
-  else if (IMDbId) {
-    data = await fetchSeason(IMDbId, season);
+    data = await Utils.searchEpisodes(APikey, IMDbId, season);
   }
   else {
-    res.json({ error: "You need to send either name or IMDb ID" })
+    res.status(400).json("Title does not exist or incorrect IMDB ID.")
   }
 
   res.json(data);
 })
 
-
-
-let fetchSeason = async (id, season) => {
-  let request = await axios.get(`https://imdb-api.com/en/API/SeasonEpisodes/${APikey}/${id}/${season}`);
-  return request.data;
-}
 
 export default router;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import axios from "axios";
 import searchMovies from "./Movies/searchMovieTitles"
 import top250 from "./Movies/top250Movies"
 import mostPopularMovies from "./Movies/mostPopular"
@@ -10,6 +11,8 @@ import searchEpisodes from "./TVShows/searchEpisodes"
 import top250TVs from "./TVShows/top250TVs"
 import seasonEpisodes from "./TVShows/searchSeasonEpisodes"
 import mostPopularTVSeries from "./TVShows/mostPopular"
+import Utils from "./Utils/callEndpointUtil"
+import possibleFilters from './Models/possibleFilters';
 
 dotenv.config();
 const app = express()
@@ -20,6 +23,48 @@ const APikey = process.env.APikey
 app.get('/', async (req, res) => {
   res.send('Test')
 })
+
+app.get('/search', async (req, res) => {
+
+  let word = req.query.expression;
+  let request = await axios.get(`https://imdb-api.com/en/API/SearchAll/${APikey}/${word}`);
+  let data = request.data;
+
+  res.json(data);
+})
+
+app.get('/info', async (req, res) => {
+
+  let data;
+  let allFilters = possibleFilters
+  let name = req.query.name;
+  let IMDbId = req.query.id;
+
+  let filters = req.query.filters
+  filters = filters.split(",");
+
+  let queryFilters = allFilters.map(x => {
+    if (filters.includes(x)) return x;
+  })
+
+  queryFilters = queryFilters.filter(x => x != null).join(",");
+
+  if (IMDbId != undefined) {
+    data = await Utils.fetchInfo(IMDbId, queryFilters);
+  }
+  else if (name) {
+    let SeriesTitle = await axios.get(`https://imdb-api.com/en/API/SearchSeries/${APikey}/${name}`);
+    IMDbId = SeriesTitle.data.results[0].id;
+
+    data = await Utils.fetchInfo(IMDbId, queryFilters);
+  }
+  else {
+    res.status(400).send()
+  }
+
+  res.json(data);
+})
+
 
 app.use("/movies", searchMovies)
 app.use("/movies", top250)
@@ -32,5 +77,6 @@ app.use("/series", searchEpisodes)
 app.use("/series", top250TVs)
 app.use("/series", seasonEpisodes)
 app.use("/series", mostPopularTVSeries)
+
 
 app.listen(port, () => console.log(`http://localhost:${port}`))
