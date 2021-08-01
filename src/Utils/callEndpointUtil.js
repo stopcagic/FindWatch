58d450d3de06864883eb108e01134ea74d0dc6cc
Saud Topcagic <Sersto@byggtjeneste.no>
2021-08-01 13:02:52 +0200

    add jw endpoinds

--- src/Utils/callEndpointUtil.js
+++ src/Utils/callEndpointUtil.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const APikey = process.env.APikey
-const imdbBaseUrl = process.env.imdbApiUrl
+const imdbBaseUrl = process.env.IMDBAPIURL
+const jwBaseUrl = process.env.JWAPIURL
+// const jwPosterUrl = process.env.JWPOSTERURL
+// const jwUserCode = process.env.JWUSERPROFILECODE
 
 const Utils = {
 
@@ -11,14 +14,29 @@ const Utils = {
     let request = await axios.get(`${imdbBaseUrl}${params}`);
     return request.data
   },
+
+  async justWatchAPIfetchData(params, body) {
+
+    let request = await axios.post(`${jwBaseUrl}${params}`, JSON.stringify(body));
+    //map svaki od data.items => item.poster = (pozovi image url)
+    return request.data
+  },
+
   async fetchID(name) {
     let SeriesTitle = await axios.get(`${imdbBaseUrl}/Search/${APikey}/${name}`);
 
     return SeriesTitle.data.results[0].id;
   },
+
   async fetchInfo(IMDbId, queryFilters) {
     let request = await axios.get(`${imdbBaseUrl}/Title/${APikey}/${IMDbId}/${queryFilters}`);
 
+    return request.data;
+  },
+
+  async fetchJWInfo(type, jw_id) {
+    let request = await axios.get(`${jwBaseUrl}/${type}/${jw_id}/locale/en_US`);
+    //map svaki od properties => poster = (pozovi image url)
     return request.data;
   }
 }
--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from 'dotenv';
-import axios from "axios";
 
 import Utils from "./Utils/callEndpointUtil"
 import tokenVerify from "./Utils/tokenVerify"
@@ -9,9 +8,7 @@ import possibleFilters from './Models/possibleFilters';
 const router = express.Router();
 dotenv.config();
 
-
 const APikey = process.env.APikey
-const imdbBaseUrl = process.env.imdbApiUrl
 
 router.get('/', async (_, res) => {
   res.send("Welcome to FindWatch")
@@ -20,15 +17,13 @@ router.get('/', async (_, res) => {
 router.get('/search', [tokenVerify], async (req, res) => {
 
   let word = req.query.expression;
-  let request = await axios.get(`${imdbBaseUrl}/SearchAll/${APikey}/${word}`);
-  let data = request.data;
+  let data = await Utils.imdbAPIfetchData(`/SearchAll/${APikey}/${word}`);
 
   res.json(data);
 })
 
 router.get('/info', [tokenVerify], async (req, res) => {
   try {
-    let data;
     let allFilters = possibleFilters
     let name = req.query.name;
     let IMDbId = req.query.id;
@@ -48,13 +43,71 @@ router.get('/info', [tokenVerify], async (req, res) => {
       throw "Title does not exist or incorrect IMDB ID."
     }
 
-    data = await Utils.fetchInfo(imdbId, orderedFilters);
+    const data = await Utils.fetchInfo(imdbId, orderedFilters);
 
     res.json(data);
 
   } catch (error) {
     res.status(400).send(error)
   }
+})
+
+router.get('/jw_info', [tokenVerify], async (req, res) => {
+  try {
+    const type = req.query.type;
+    const jw_id = req.query.jw_id;
+
+    if (type == undefined && jw_id == undefined) throw "Missing type or id."
+
+    const data = await Utils.fetchJWInfo(type, jw_id)
+
+    res.json(data);
+
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+router.post('/filter', [tokenVerify], async (req, res) => {
+  try {
+    const options = req.body
+    if (options == undefined) throw "Body cannot be empty."
+
+    const body = {
+      "age_certifications": null,
+      "content_types": null,
+      "presentation_types": null,
+      "providers": null,
+      "genres": null,
+      "languages": null,
+      "release_year_from": null,
+      "release_year_until": null,
+      "monetization_types": null,
+      "min_price": null,
+      "max_price": null,
+      "scoring_filter_types": null,
+      "cinema_release": null,
+      "query": null,
+      "page": null,
+      "page_size": null
+    };
+    const paramKeys = Object.keys(body);
+
+    for (const key in options) {
+      if (paramKeys.indexOf(key) === -1) {
+        throw "invalid option '" + key + "'";
+      }
+      else {
+        body[key] = options[key];
+      }
+    }
+    const data = await Utils.justWatchAPIfetchData(`/en_US/popular`, body)
+
+    res.send(data)
+  }
+  catch (error) {
+    res.status(400).send(error)
+  }
+
 })
 
 export default router
